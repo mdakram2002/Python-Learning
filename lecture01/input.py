afe33b0170d6bdbd449423d0c82d413b3f3f05fc
Md <mdakram12022002@gmail.com>
2024-09-10 15:24:50 +0530

    some file are added and deleted

---
+++ lecture01/input.py
@@ -0,0 +1,18 @@
+name = input("Enter Your Name: ")
+print(name)
+
+
+#Here Using type Casting String to intiger
+age = int(input("Enter Your age: "))
+print(type(age))
+
+
+#Addition of two variable via tpyeCasting
+num1 = int(input("Enter Num one: "))
+num2 = int(input("Enter Num second: "))
+
+print("Sum of these two number is: ", num1 + num2 )
+
+#initializing with other variable sum
+sum = num1 + num2
+print("Sum of these two number is: ", sum )
